b5ab0c7164348d6ef9a8f1fdaf6620e5075029cf
HimbeerserverDE <himbeerserverde@gmail.com>
2023-05-15 19:12:23 +0200

    fix unit test

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ mod tests {
         let mut buf = Vec::new();
         foo.serialize(&mut buf)?;
 
-        assert_eq!(&buf, &[42]);
+        assert_eq!(&buf, &1337_i32.to_be_bytes());
         Ok(())
     }
 }
