13a05ba9735091af9340d86bc64b7b7769af2e4b
HimbeerserverDE <himbeerserverde@gmail.com>
2023-05-15 19:11:39 +0200

    derive Serialize for i32

--- src/ser.rs
+++ src/ser.rs
@@ -5,3 +5,10 @@ use std::io::Write;
 pub trait Serialize {
     fn serialize<W: Write>(&self, w: &mut W) -> Result<()>;
 }
+
+impl Serialize for i32 {
+    fn serialize<W: Write>(&self, w: &mut W) -> Result<()> {
+        w.write_all(&self.to_be_bytes())?;
+        Ok(())
+    }
+}
