5d3364ad97c0c6c2dd9d1afc0be6158ca863fbde
HimbeerserverDE <himbeerserverde@gmail.com>
2023-05-16 16:08:48 +0200

    implement integer deserialization

--- src/de.rs
+++ src/de.rs
@@ -1,7 +1,26 @@
 use crate::Result;
 
 use std::io::Read;
+use std::mem;
 
 pub trait Deserialize {
-    fn deserialize<R: Read>(&mut self, r: R) -> Result<()>;
+    fn deserialize<R: Read>(&mut self, r: &mut R) -> Result<()>;
 }
+
+macro_rules! impl_deserialize {
+    ($($t:ty) *) => {
+        $(
+            impl Deserialize for $t {
+                fn deserialize<R: Read>(&mut self, r: &mut R) -> Result<()> {
+                    let mut buf = [0; mem::size_of::<$t>()];
+                    r.read_exact(&mut buf)?;
+
+                    *self = <$t>::from_be_bytes(buf);
+                    Ok(())
+                }
+            }
+        )*
+    };
+}
+
+impl_deserialize!(i8 i16 i32 i64 i128 u8 u16 u32 u64 u128);
--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@ mod ser;
 
 #[cfg(test)]
 mod tests {
-    use super::{ser::Serialize, *};
+    use super::{de::Deserialize, ser::Serialize, *};
 
-    use ppproperly_macros::Serialize;
+    use ppproperly_macros::{Deserialize, Serialize};
 
     #[test]
     fn test_derive_serialize() -> Result<()> {
@@ -23,6 +23,22 @@ mod tests {
         foo.serialize(&mut buf)?;
 
         assert_eq!(&buf, &1337_i32.to_be_bytes());
+        Ok(())
+    }
+
+    #[test]
+    fn test_derive_deserialize() -> Result<()> {
+        #[derive(Deserialize)]
+        struct Foo {
+            bar: i32,
+        }
+
+        let mut foo = Foo { bar: 0 };
+
+        let buf = 1337_i32.to_be_bytes();
+        foo.deserialize(&mut buf.as_ref())?;
+
+        assert_eq!(foo.bar, 1337);
         Ok(())
     }
 }
