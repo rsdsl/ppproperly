rsdsl/ppproperly

commit c83af6a74c451d7ba8613105c1df5492123bac2d

don't ignore true EOF errors in Vec<PPPoETag> deserializer

src/pppoe.rs
@@ -1,6 +1,6 @@
 use crate::{Deserialize, Error, Result, Serialize, VerType};
 
-use std::io::{self, Read, Take, Write};
+use std::io::{Read, Take, Write};
 
 use ppproperly_macros::{Deserialize, Serialize};
 
@@ -363,23 +363,11 @@ impl Serialize for [PPPoETag] {
 
 impl Deserialize for Vec<PPPoETag> {
     fn deserialize<R: Read>(&mut self, r: &mut R) -> Result<()> {
-        loop {
+        while r.bytes().count() > 0 {
             let mut tmp = PPPoETag::from(PPPoETagPayload::EndOfList);
 
-            match tmp.deserialize(r) {
-                Ok(_) => self.push(tmp),
-                Err(e) => {
-                    if let Error::Io(ref ioe) = e {
-                        if ioe.kind() == io::ErrorKind::UnexpectedEof {
-                            break;
-                        } else {
-                            return Err(e);
-                        }
-                    } else {
-                        return Err(e);
-                    }
-                }
-            }
+            tmp.deserialize(r)?;
+            self.push(tmp);
         }
 
         Ok(())

commit 3058b395c88b1a51c0d823a2953648ed77d59c0a

actually fix EOF errors caused by c83af6a74c451d7ba8613105c1df5492123bac2d without consuming the bytes in the check

src/pppoe.rs
@@ -363,7 +363,7 @@ impl Serialize for [PPPoETag] {
 
 impl Deserialize for Vec<PPPoETag> {
     fn deserialize<R: Read>(&mut self, r: &mut R) -> Result<()> {
-        while r.bytes().count() > 0 {
+        while r.bytes().size_hint().0 > 0 {
             let mut tmp = PPPoETag::from(PPPoETagPayload::EndOfList);
 
             tmp.deserialize(r)?;