43b138eeec3540660bec582e01f6d235f99a7aab
HimbeerserverDE <himbeerserverde@gmail.com>
2023-07-28 14:55:35 +0200

    print mac addresses in hex

--- src/pppoe.rs
+++ src/pppoe.rs
@@ -43,7 +43,7 @@ impl Display for MacAddr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{}:{}:{}:{}:{}:{}",
+            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
             self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
         )
     }
